ea80727e96a8d459b6b21d7f1c574b18823dcea2
YURACHAE\yurachae <yura920501@gmail.com>
2022-06-14 09:54:26 +0900

    스프링 데이터 JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,38 +1,31 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
     //직접 스프링 빈 등록
-    private EntityManager em;
 
-    @Autowired
-    public SpringConfig(EntityManager em){
-        this.em = em;
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-        //  OCP (Open-Closed Princlple) 개방 폐쇄 원칙
+//    @Bean
+//    public MemberRepository memberRepository() {
+    //  OCP (Open-Closed Princlple) 개방 폐쇄 원칙
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(datasource);
 //        return new JdbcTemplateMemberRepository(datasource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 }
